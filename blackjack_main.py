ec3122603a83df06759cf965c02b5775e4e4ef06
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-04 12:07:17 +0800

    need hit or stand functionality

--- blackjack_main.py
+++ blackjack_main.py
@@ -1,29 +1,45 @@
 
 import art
 import random as rd
-import cards as c
-import player_draw as pd
 
-def cards():
-    dealer_cards = [] 
-    for i in range(2):
-        dealt = rd.choice(c.cards)
-        dealer_cards.append(dealt)
-    if sum(dealer_cards)<16:
-        dealt = rd.choice(c.cards)
-        dealer_cards.append(dealt)
-    # print(dealer_cards)
-    return dealer_cards
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+def draw_cards(bot):
+    """ Draws cards from the given card list and returns a list called hand"""
+    hand = []
 
-def computer_hand(dealt = []):
-    hand = sum(dealt)
-    if hand>21:
-        print("Dealer's a Bust")
-        return hand
-    else:
-        return hand
+    for i in range (2):
+        dealt = rd.choice(cards)
+        hand.append(dealt)
+    dealer = True
+    if dealer == bot:
+        while 16>sum(hand)<21:
+            dealt = rd.choice(cards)
+            hand.append(dealt)
+
+    # to work on ... the user must fetch the damn deck & return a respond if he wants to hit or stand
+    if dealer != bot:
+        rep = True
+        while rep:
+            if sum(hand)<21:
+                add = input("Do you want another card? (Hit) Yes[y] or (Stand) No?[n]: ")
+                if add == "y":
+                    hit = rd.choice(cards)
+                    hand.append(hit)
+                    print("-"*80)
+                    print(f"Here is another card: {hit} \nYour current hand is: {hand}")
+                    rep = True
+                elif add == "n":
+                    print(f"Your current hand is: {hand}")
+                    rep = False
+            else:
+                print("Hmmm none of the above choices ... continuing for now...")
+            
+    if sum(hand)>21 and 11 in hand:
+        hand[hand.index(11)] = 1
     
+    return hand
+   
 def check_both_hands(com_hand, pos_hand):
     if pos_hand<com_hand<21:
         print("Dealer Wins")
@@ -39,23 +55,26 @@ def check_both_hands(com_hand, pos_hand):
         print("Dealer Bust: Player wins")
     elif pos_hand> 21:
         print("Player Bust: Dealer wins")
-    
-    
-
-print(art.logo)
 
 restart = True
 while restart:
-    hidden_cards = len(cards())-1 
-    print(f"Dealer shows first card: [{cards()[0]}]","[]"*hidden_cards)
+    print(art.logo)
     print("="*80)
 
-    dealer = computer_hand(cards())
-    player = pd.player_hand()
-    
-    check_both_hands(dealer, player)
 
-    print(f"Dealer's Cards are: {cards()}")
+    dealer = draw_cards(bot=True)
+    mul = len(dealer)-1
+    print(f"Dealers Cards: {dealer[0]}","[_]"*mul)
+    dealer_hands = sum(dealer)
+
+    player = draw_cards(bot=False)
+    print(f"Player Cards: {player}")
+    players_hands = sum(player)
+    print(f"Player's Hand: {players_hands}")
+       
+    check_both_hands(dealer_hands,players_hands)
+    print(f"Player's Hand: {dealer}")
+
 
     q = input("Want to play again? Type any letter to play another round, press [n] to Exit:\n")
     if q == "n":
